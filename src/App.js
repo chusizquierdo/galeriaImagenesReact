ff378dc78c54fa31baef99b8b69efe029f1fe6c5
chusULPGC <jesusizquierdopuebla@gmail.com>
2021-12-05 01:10:22 +0000

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from 'formik'
+import { useState } from 'react';
+import './App.css'
+
+
+const styles = {
+  etiqueta: {
+    color: 'white'
+  }
+}
 
 function App() {
+  const [photos, setPhotos] = useState([])
+  const open = (urls) => {
+    window.open(urls)
+  }
+  console.log({ photos })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers: {
+                'Authorization': 'Client-ID -5MzyRJFiX1IEki6m_5uzw4QkrRolu_0VdNNdH54h10'
+              }
+            })
+            const data = await response.json()
+            const image = data.results[0].links.html;
+            setPhotos(data.results)
+            //llamar a la api de unplash
+            // data.results.map(x=>{
+            //   console.log(x.links.html)
+            // })            
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <label style={styles.etiqueta}>Buscador de imagenes </label>
+            <Field name="search" />
+          </Form>
+        </Formik>
       </header>
+      <div className='container'>
+        <div className='center'>
+
+
+          {photos.map(photo =>
+
+            //si pinchas en la imagen te lleva a la url
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <p>{photo.description + ' -  ' + photo.alt_description}</p>
+              <img src={photo.urls.regular} />
+            </article>
+          )}
+        </div>
+
+
+      </div>
     </div>
   );
 }
